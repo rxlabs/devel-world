2cafc2d598e5e26e2926e947bf8b94641b3551c6
Evan Sosenko <razorx@evansosenko.com>
2026-01-19 22:44:18 -0800

    Always use Hello, World!

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,6 @@ const rootElement = document.getElementById('root')
 
 const newElement = document.createElement('h1')
 
-newElement.innerText = 'Hello, world!'
+newElement.innerText = 'Hello, World!'
 
 rootElement.appendChild(newElement)
